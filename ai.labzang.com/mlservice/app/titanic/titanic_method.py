bbd4c29f0655f31a753c33df5b572b5a648c075c
dpthf001107 <dpthf001107@gmail.com>
2025-12-05 18:06:33 +0900

    .

--- ai.labzang.com/mlservice/app/titanic/titanic_method.py
+++ ai.labzang.com/mlservice/app/titanic/titanic_method.py
@@ -3,4 +3,18 @@
 class TitanicMethod(object): 
 
     def __init__(self):
+        # 데이터셋 객체 생성
+        pass
+
+
+    def new_model(self):
+        # train.csv 파일을 읽어와서 데이터프레임 작성
+        pass
+
+    def create_train(self):
+        # Survived 값을 제거한 데이터프레임 작성
+        pass
+
+    def create_label(self):
+        # Survived 값만 가지는 답안지 데이터프레임 작성
         pass
